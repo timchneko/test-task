80c9738c6c2331d9afc7f828b42c81ca1cfaba76
timchneko <l349@ricchan.Dlink>
2016-11-13 14:43:04 +1000

    model

---
+++ backend/model.php
@@ -0,0 +1,108 @@
+<?php
+	class Model {
+		private $id;
+		private $name;
+		private $email;
+		private $text;
+		private $datetime;
+		private $imagePath;
+		private $isApproved = 0;
+		private $isChanged = 0;
+
+ 		public function load($id = "") {
+   			if (!is_numeric($id)) {
+   				throw new Exception('Некорректный id!');
+   			}
+			$db = Connection::getInstance();
+			$conn = $db->getConnection();
+			$query = "SELECT * FROM posts WHERE id = :id";
+			$stmnt = $conn->prepare($query);
+			$stmnt->bindValue(':id', $this->id);
+			$stmnt->execute();
+			$result = $stmnt->fetch();
+			return $result;
+   		}
+
+   		public function save() {
+			$db = Connection::getInstance();
+			$conn = $db->getConnection();
+			if (!$this->id) {
+				$query = "INSERT INTO posts(name, email, text, imagePath, isApproved, isChanged) "
+							."VALUES (:name, :email, :text, :imagePath, :isApproved, :isChanged)";
+			} else {
+				$query = "UPDATE posts SET name = :name, email = :email, text = :text, imagePath = :imagePath, "
+								."isApproved = :isApproved, isChanged = :isChanged)"
+								."WHERE id = :id";
+			}
+			$stmnt = $conn->prepare($query);
+			$stmnt->bindValue(':name', $this->name);
+			$stmnt->bindValue(':email', $this->email);
+			$stmnt->bindValue(':text', $this->text);
+			$stmnt->bindValue(':imagePath', $this->imagePath);
+			$stmnt->bindValue(':isApproved', $this->isApproved);
+			$stmnt->bindValue(':isChanged', $this->isChanged);
+			if ($this->id) {
+				$stmnt->bindValue(':id', $this->id);
+			}
+			$conn->beginTransaction();
+			$stmt1 = $stmnt->execute();
+			if (!$this->id) {
+				$this->id = $conn->lastInsertId();
+			}
+			$conn->commit();
+   		}
+
+   		public function setName($name) {
+   			if (len($name) > 100) {
+   				throw new Exception('Имя не может превышать 100 символов!');
+   			}
+   			$this->name = $name;
+   		}
+
+   		public function setEmail($email) {
+   			if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+   				throw new Exception('Некорректный email!');
+   			}
+   			$this->$email = $email;
+   		}
+
+   		public function setText($text) {
+   			if (len($name) > 10000) {
+   				throw new Exception('Сообщение не может превышать 10000 символов!');
+   			}
+   			$this->text = $text;
+   		}
+
+   		public function getId() {
+   			return $this->id;
+   		}
+
+   		public function getName() {
+   			return $this->name;
+   		}
+
+   		public function getEmail() {
+   			return $this->email;
+   		}
+
+   		public function getText() {
+   			return $this->text;
+
+   		public function getDatetime() {
+   			return $this->datetime;
+   		}
+
+   		public function getImagePath() {
+   			return $this->imagePath;
+   		}
+
+   		public function getIsApproved() {
+   			return $this->isApproved;
+   		}
+
+   		public function getIsChanged() {
+   			return $this->isChanged;
+   		}
+
+	}
+?>
